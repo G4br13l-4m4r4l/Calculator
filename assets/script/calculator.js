8954bedcf659fe92bc77162ffa90cabc8521b94c
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2022-08-08 21:49:34 -0300

    Adding the buttons

--- assets/script/calculator.js
+++ assets/script/calculator.js
@@ -1,3 +1,16 @@
 const previousOperationText = document.querySelector("#previous-operation");
 const currentOperationText = document.querySelector("#current-operation");
 const button = document.querySelectorAll("#buttons-container button");
+
+class Calculator{
+
+}
+
+//geting the buttons
+button.forEach((btn) => {
+    btn.addEventListener("click", (e) =>{
+        const value = e.target.innerText;
+
+        console.log(value);
+    })
+});
