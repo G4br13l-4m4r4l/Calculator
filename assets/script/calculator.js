399792b0ce6a307224e81e6d745ae2b0608bfee3
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2022-08-15 21:27:54 -0300

    adding the operational functions to some buttons

--- assets/script/calculator.js
+++ assets/script/calculator.js
@@ -22,11 +22,19 @@ class Calculator{
 
     //process all operations from the calculator
     processOperation(operation){
+        //check if it is empty
 
+        if(this.currentOperationText.innerText === ""){
+            //change the operation
+            if(this.previousOperationText.innerText !== ""){
+                this.changeOperation(operation);
+            }
+            return;
+        }
 
         //getting the values
         let operationValue;
-        let previous = +this.previousOperationText.innerText;
+        let previous = +this.previousOperationText.innerText.split(" ")[0];
         let current = this.currentOperationText.innerText;
 
         switch(operation){
@@ -34,6 +42,22 @@ class Calculator{
                 operationValue = current + current;
                 this.updateScreen(operationValue, operation, current, previous);
                 break;
+
+            case "-":
+                operationValue = current - current;
+                this.updateScreen(operationValue, operation, current, previous);
+                break;
+            
+            case "*":
+                operationValue = current * current;
+                this.updateScreen(operationValue, operation, current, previous);
+                break;
+
+            case "/":
+                operationValue = current / current;
+                this.updateScreen(operationValue, operation, current, previous);
+                break;
+
             default:
                 return;
 
@@ -44,8 +68,34 @@ class Calculator{
 
     //changing the values
     updateScreen(operationValue = null, operation = null, current = null, previous = null){
+        
+        if(operationValue === null){
         this.currentOperationText.innerText += this.currentOperation;
+        }else{
+            //check if its zero
+            if(previous ===0){
+                operationValue = current;
+            }
+            //+ current value into the previous
+
+            this.previousOperationText.innerText = `${operationValue} ${operation}`;
+            this.currentOperationText.innerText = "";
+        }
+
     }
+
+    //changing the math operations
+
+    changeOperation(operation){
+
+        const mathOperation = ["*", "/", "+", "-"];
+
+        if(!mathOperation.includes(operation)){
+            return;
+        }
+        this.previousOperationText.innerText = this.previousOperationText.innerText.slice(0, -1) + operation;
+    }
+
 }
 
 const calc = new Calculator(previousOperationText, currentOperationText);
